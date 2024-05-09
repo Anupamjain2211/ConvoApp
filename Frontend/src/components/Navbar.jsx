aadff90901a2732ae7b7240f5c236759a9741569
anupam <anupamjain177@gmail.com>
2024-05-09 14:14:00 -0400

    first commit

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="max-w-screen-2xl mx-auto container px-6 md:px-40 shadow-lg h-16">
+        <div className="flex justify-between">
+          <h1>WordToPdf</h1>
+          <h1>Home</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
