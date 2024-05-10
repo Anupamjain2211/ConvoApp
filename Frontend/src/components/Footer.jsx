00a44a1460b250d470cde45db224efe95c2e0999
anupam <anupamjain177@gmail.com>
2024-05-10 14:18:44 -0400

    updated

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const footer = () => {
       <div className="item-center justify-center">
         <hr className=" border-black" />
         <h1 className="text-center py-3 text-sm">
-          Copyright &copy; 2024 - All right reserved by Shanu Softwares
+          Copyright &copy; 2024 - All right reserved by Shanu Pvt. Ltd
         </h1>
       </div>
     </>
